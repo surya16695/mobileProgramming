6ec637cf7fb76473ea20d4a832bd74046b20e996
Surya <suryasaginala@msitprogram.net>
2019-03-20 08:07:19 +0530

    A38

---
+++ Mod-4/A38/Exercise-CreateEntity/T09b.01-Exercise-CreateEntity/app/src/testDebug/aidl/com/example/android/todolist/database/PrimaryKey.java
@@ -0,0 +1,4 @@
+package com.example.android.todolist.database;
+
+@interface PrimaryKey {
+}
---
+++ Mod-4/A38/Exercise-CreateEntity/T09b.01-Exercise-CreateEntity/app/src/testDebug/aidl/com/example/android/todolist/database/TaskEntryTest.groovy
@@ -0,0 +1,4 @@
+package com.example.android.todolist.database
+
+class TaskEntryTest extends GroovyTestCase {
+}
